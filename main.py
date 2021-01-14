237e0d6e378ade5fcd555b00339f28fa101ef3d3
AbouTheGreat <72754592+AbouTheGreat@users.noreply.github.com>
2021-01-14 13:26:18 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 while True:
-    print(input.sound_level())
-    if 20 < input.sound_level():
-        light.show_animation(light.rainbowAnimation, 500)
-        pause(5000)
-      
+    print(input.light_level())
+    if 110 < input.sound_level():   
+        music.set_volume(100)
+        music.pew_pew.play()
+        
--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 while (true) {
-    console.log(input.soundLevel())
-    if (20 < input.soundLevel()) {
-        light.showAnimation(light.rainbowAnimation, 500)
-        pause(5000)
+    console.log(input.lightLevel())
+    if (110 < input.soundLevel()) {
+        music.setVolume(100)
+        music.pewPew.play()
     }
     
 }
