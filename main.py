b40e9ed5f5a9e1219dc576e80d0fbbfaa774a06a
AbouTheGreat <72754592+AbouTheGreat@users.noreply.github.com>
2021-01-19 15:12:14 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 while True:
-    print(input.light_level())
-    if 100 < input.light_level():   
-        music.set_volume(1000)
-        music.pew_pew.play_until_done()
-        
+    music.set_volume(100000)
+    light.set_all(color.rgb(255, 255, 255))
+    music.play_melody("D C# B F# F# F# F# F# F# B B B B A B B G G G G G B B B B B C# D D A A A A A A D D D D E E E C# D C# B F# F# F# F# F# F# B B B B A B B G G G G G B B B B B C# D D A A A A A A D D D D E E E C# E E F# E F# E F# E F# F# E F# E F# G G G D D D G G G G G A G G F# F# F# F# F# F# A G F# F# E E E E E E E E E C# D F# E F# E F# E F# F# E F# E F# G G G D D D G G G G G A G G F# F# F# F# F# F# A G F# F# E E", 300)
+    
--- main.ts
+++ main.ts
@@ -1,8 +1,5 @@
 while (true) {
-    console.log(input.lightLevel())
-    if (100 < input.lightLevel()) {
-        music.setVolume(1000)
-        music.pewPew.playUntilDone()
-    }
-    
+    music.setVolume(100000)
+    light.setAll(color.rgb(255, 255, 255))
+    music.playMelody("D C# B F# F# F# F# F# F# B B B B A B B G G G G G B B B B B C# D D A A A A A A D D D D E E E C# D C# B F# F# F# F# F# F# B B B B A B B G G G G G B B B B B C# D D A A A A A A D D D D E E E C# E E F# E F# E F# E F# F# E F# E F# G G G D D D G G G G G A G G F# F# F# F# F# F# A G F# F# E E E E E E E E E C# D F# E F# E F# E F# F# E F# E F# G G G D D D G G G G G A G G F# F# F# F# F# F# A G F# F# E E", 300)
 }
